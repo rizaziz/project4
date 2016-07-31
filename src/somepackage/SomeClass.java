e585d271433a5f95435474a0ea46a0adda1d85da
riz <riz_aziz2002@mail.ru>
2016-07-31 10:09:59 -0400

    initial commit

---
+++ src/somepackage/SomeClass.java
@@ -0,0 +1,13 @@
+package somepackage;
+
+public class SomeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jello");
+		System.out.println("Second hello");
+		System.out.println("Third hello");
+
+	}
+
+}
